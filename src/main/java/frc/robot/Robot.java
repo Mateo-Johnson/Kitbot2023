ae95c3da5c537cc9f46d3fb66cbdc74903c4360c
Mateo-Johnson <johnson.12@themetroschool.org>
2023-03-13 13:02:01 -0400

    second vision movement test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,13 +18,30 @@ import edu.wpi.first.networktables.NetworkTableInstance;
  */
 public class Robot extends TimedRobot {
 
-  // Constants for PID control
-  private final double kP = 0.1;
-  private final double kI = 0.0;
-  private final double kD = 0.0;
+    // Constants for PID control
+    private final double kP = 0.1;
+    private final double kI = 0.0;
+    private final double kD = 0.0;
+    private final double kTolerance = 1.0;
 
-  private double integralError = 0.0;
-  private double previousError = 0.0;
+    private double integralError = 0.0;
+    private double previousError = 0.0;
+
+    // Constants for Limelight camera
+    private final double kImageWidth = 320.0; // Width of the camera image in pixels
+    private final double kImageHeight = 240.0; // Height of the camera image in pixels
+    private final double kVerticalFOV = 49.7; // Vertical field of view of the camera in degrees
+    private final double kTagSize = 0.1778; // Size of the AprilTag in meters (7 inches)
+
+    double calculateDistanceFromArea(double area) {
+      // Calculate the height of the target in pixels based on the target area
+      double targetHeight = 2.0 * kTagSize / Math.sqrt(area);
+      
+      // Calculate the distance to the target based on the height and the camera's vertical FOV
+      double distance = kImageHeight / (2.0 * targetHeight * Math.tan(Math.toRadians(kVerticalFOV / 2.0)));
+      
+      return distance;
+  }
 
 
 
@@ -55,33 +72,58 @@ public class Robot extends TimedRobot {
 
 
 
+  
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
 
+    
+
     double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
     double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
     double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+    double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
+    double ts = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ts").getDouble(0);
 
 
     if (tv == 1){
+
+     // Calculate distance to target based on target area
+     double targetDistance = calculateDistanceFromArea(ta);
+
+    // Calculate target position relative to robot
+    double targetX = targetDistance * Math.sin(Math.toRadians(tx));
+    double targetY = targetDistance * Math.sin(Math.toRadians(ty));
+
+    // Calculate error in target position
+    double targetXError = 0.0; // Set desired target X position here
+    double targetYError = 0.0; // Set desired target Y position here
+    double xError = targetX - targetXError;
+    double yError = targetY - targetYError;
+
     // PID control for steering
-    double error = -tx;
-    integralError += error;
-    double derivativeError = error - previousError;
-    double steeringAdjustment = kP * error + kI * integralError + kD * derivativeError;
+    double steeringError = -tx;
+    integralError += steeringError;
+    double derivativeError = steeringError - previousError;
+    double steeringAdjustment = kP * steeringError + kI * integralError + kD * derivativeError;
 
     // PID control for distance
-    double distanceError = -ty;
+    double distanceError = targetDistance - kTolerance;
     double distanceAdjustment = kP * distanceError;
 
-    robotDrive.arcadeDrive(distanceAdjustment, steeringAdjustment);
+    // PID control for X position
+    double xAdjustment = kP * xError;
 
-    previousError = error;
+    // PID control for Y position
+    double yAdjustment = kP * yError;
 
-    } 
+    robotDrive.arcadeDrive(distanceAdjustment + xAdjustment, steeringAdjustment + yAdjustment);
+
+      
+    }
+
 
     if (tv == 0) {
 
@@ -91,7 +133,6 @@ public class Robot extends TimedRobot {
       robotDrive.arcadeDrive(speed, steering);
 
     }
-    
 
     }
      
