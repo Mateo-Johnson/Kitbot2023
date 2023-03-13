6875ef17c9c18d52fd9c484e0e32bb19c2d42a49
Mateo-Johnson <johnson.12@themetroschool.org>
2023-03-13 12:26:04 -0400

    vison based movement v1

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,16 +2,13 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
-
 package frc.robot;
-
-
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 
 
@@ -21,14 +18,27 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
  */
 public class Robot extends TimedRobot {
 
-  private final WPI_TalonSRX m_leftMotor1 = new WPI_TalonSRX(1);
-  private final WPI_TalonSRX m_rightMotor1 = new WPI_TalonSRX(2);
-  private final WPI_TalonSRX m_leftMotor2 = new WPI_TalonSRX(3);
-  private final WPI_TalonSRX m_rightMotor2 = new WPI_TalonSRX(4);
+  // Constants for PID control
+  private final double kP = 0.1;
+  private final double kI = 0.0;
+  private final double kD = 0.0;
+
+  private double integralError = 0.0;
+  private double previousError = 0.0;
+
+
+
+  
+
+  public static final WPI_TalonSRX m_leftMotor1 = new WPI_TalonSRX(2);
+  public static final WPI_TalonSRX m_rightMotor1 = new WPI_TalonSRX(3);
+  public static final WPI_TalonSRX m_leftMotor2 = new WPI_TalonSRX(4);
+  public static final WPI_TalonSRX m_rightMotor2 = new WPI_TalonSRX(5);
 
   private final DifferentialDrive robotDrive = new DifferentialDrive(m_leftMotor1, m_rightMotor1);
   
   public final static CommandXboxController primaryDriver = new CommandXboxController(0);
+
 
   @Override
   public void robotInit() {
@@ -36,19 +46,57 @@ public class Robot extends TimedRobot {
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to inve`rt the left side instead.
     m_rightMotor1.setInverted(true);
+    m_rightMotor1.setInverted(false);
 
     m_rightMotor2.follow(m_rightMotor1);
     m_leftMotor2.follow(m_leftMotor1);
 
-    
-
   }
+
+
 
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    robotDrive.arcadeDrive(primaryDriver.getLeftY(), primaryDriver.getRightX());
+
+    double tx = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
+    double ty = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
+    double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
+
+
+    if (tv == 1){
+    // PID control for steering
+    double error = -tx;
+    integralError += error;
+    double derivativeError = error - previousError;
+    double steeringAdjustment = kP * error + kI * integralError + kD * derivativeError;
+
+    // PID control for distance
+    double distanceError = -ty;
+    double distanceAdjustment = kP * distanceError;
+
+    robotDrive.arcadeDrive(distanceAdjustment, steeringAdjustment);
+
+    previousError = error;
+
+    } 
+
+    if (tv == 0) {
+
+      double speed = primaryDriver.getRightY();
+      double steering = primaryDriver.getLeftX();
+
+      robotDrive.arcadeDrive(speed, steering);
+
+    }
+    
+
+    }
+     
+
   }
-}
+
+
+
